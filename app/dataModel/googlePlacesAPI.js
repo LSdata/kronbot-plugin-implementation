93ab893a25ed21abf1bbd8c6a6e11dbaa9424e89
Linnea Strågefors <ls223aa@student.lnu.se>
2016-12-21 11:42:50 +0000

    3 items

--- app/dataModel/googlePlacesAPI.js
+++ app/dataModel/googlePlacesAPI.js
@@ -24,43 +24,30 @@ module.exports = {
       response.on('end', function() {
         
        generatePlaceArr(data, function(arr) {
-          
-          //5 callbacks to get photo and add to array
+          //get photo and add to array
           getPlacePhoto(arr[0][3], function(photo_ref0) {
               arr[0][3] = photo_ref0;
-          });
-          getPlacePhoto(arr[1][3], function(photo_ref1) {
+              getPlacePhoto(arr[1][3], function(photo_ref1) {
                 arr[1][3] = photo_ref1;
-          });
-          getPlacePhoto(arr[2][3], function(photo_ref2) {
-            arr[2][3] = photo_ref2;
-          });
-          getPlacePhoto(arr[3][3], function(photo_ref3) {
-            arr[3][3] = photo_ref3;
-          });
-          getPlacePhoto(arr[4][3], function(photo_ref4) {
-            arr[4][3] = photo_ref4;
-          });
-          
-          //get place websites from google place detail search by place ID
-          getPlaceWebsite(arr[0][6], function(website) {
-            arr[0][6] = website;
-            getPlaceWebsite(arr[1][6], function(website) {
-                arr[1][6] = website;
-                getPlaceWebsite(arr[2][6], function(website) {
-                  arr[2][6] = website;
-                  getPlaceWebsite(arr[3][6], function(website) {
-                    arr[3][6] = website;
-                    getPlaceWebsite(arr[4][6], function(website) {
-                      arr[4][6] = website;
-                      createMess(arr, function(mess) {
-                        return callback(mess)
+                getPlacePhoto(arr[2][3], function(photo_ref2) {
+                  arr[2][3] = photo_ref2;
+
+                  //get place websites from google place detail search by place ID
+                  getPlaceWebsite(arr[0][6], function(website) {
+                    arr[0][6] = website;
+                    getPlaceWebsite(arr[1][6], function(website) {
+                      arr[1][6] = website;
+                      getPlaceWebsite(arr[2][6], function(website) {
+                        arr[2][6] = website;
+                            createMess(arr, function(mess) {
+                              return callback(mess)
+                            });
                       });
                     });
                   });
                 });
-            });
-          });
+              });
+          });          
         });
       });
     }).on('error', function(e) {
@@ -75,6 +62,7 @@ function generatePlaceArr(data, callback){
   var len = parsed['results'].length;
   var counter = -1; //init
   var flagFirst = 0;
+  //console.log(data);
 
   //get 7 google place items. Place in array.
   for(var i=0; i<len; i++){
@@ -88,10 +76,13 @@ function generatePlaceArr(data, callback){
       var images = parsed['results'][i].photos;
       var categTypes = parsed['results'][i].types;
       var placeID = parsed['results'][i].place_id;
+      console.log(images);
       
-      if(images && categTypes && name && address && lat && lng && counter <4 && placeID){
+      if( (typeof images !== 'undefined') && categTypes && name && address && lat && lng && counter <4 && placeID){
         counter++;
+        //console.log("IMAGES: "+ images);
         ref = parsed['results'][i].photos[0].photo_reference;
+        console.log("REF: "+ ref);
         
         //get place categories
         var typesArr = parsed['results'][i].types;
@@ -102,7 +93,8 @@ function generatePlaceArr(data, callback){
         }
         type = type.substring(0, type.length - 2); //remove last ', '
         placeArr[counter] = [name, type, address, ref, lat, lng, placeID];
-      }
+      }else
+        continue;
       
     }
     return callback(placeArr);
@@ -147,26 +139,6 @@ function createMess(placeArr, callback){
               type: "web_url",
               url: "http://maps.google.com/maps?q=loc:"+placeArr[2][4] +","+placeArr[2][5],
               title: placeArr[2][2]
-            }]
-            }, {
-            title: placeArr[3][0],
-            image_url: placeArr[3][3], 
-            subtitle: placeArr[3][1],
-            item_url: placeArr[3][6],
-            buttons: [{
-              type: "web_url",
-              url: "http://maps.google.com/maps?q=loc:"+placeArr[3][4] +","+placeArr[3][5],
-              title: placeArr[3][2]
-            }]
-            }, {
-            title: placeArr[4][0],
-            image_url: placeArr[4][3], 
-            subtitle: placeArr[4][1],
-            item_url: placeArr[4][6],
-            buttons: [{
-              type: "web_url",
-              url: "http://maps.google.com/maps?q=loc:"+placeArr[4][4] +","+placeArr[4][5],
-              title: placeArr[4][2]
             }]
             }
           ]
